7384f6ae5e4cb5ada685523c96440b3b8d915f89
Yael-Millan <elvinyael16@gmail.com>
2024-02-21 09:20:17 -0400

    base

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print if the number is positive, zero, or negative
  *
@@ -8,10 +9,13 @@
  */
 int main(void)
 {
-char c;
-for (c = '0'; c <= '9'; c++)
-{
-        putchar(c);
+char letter; 
+
+for (i = 0 ; i < 10 ; i++)
+putchar((i % 10) + '0');
+for (letter = 'a'; letter <= 'f'; letter++)
+putchar(letter);
+ 
 }
 putchar('\n');
 return (0);
