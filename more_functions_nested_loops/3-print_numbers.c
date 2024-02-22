fa089b7fd9e7b52a8cdf57048dc1a6c731a1388f
Yael-Millan <elvinyael16@gmail.com>
2024-02-22 14:41:31 -0400

    3

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,17 +1,32 @@
 #include "main.h"
 
 /**
- * print_numbers - Entry point
- *
- * Return: void
+ * print_numbers - prints number
  */
+
 void print_numbers(void)
 {
 int i;
 
-for (i = 0; i <= 9; i++)
+for (i = 48; i < 58; i++)
 {
 _putchar(i);
 }
 _putchar('\n');
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
