125273fb90da29d3473ae879cdf21e1cba30c8e9
Yael-Millan <elvinyael16@gmail.com>
2024-02-22 13:46:38 -0400

    1

--- more_functions_nested_loops/1-isdigit.c
+++ more_functions_nested_loops/1-isdigit.c
@@ -1,15 +1,28 @@
 #include "main.h"
 
 /**
- * _isdigit - Entry point
- *@c: charater to check
- * Return: if is digit  or not, return 1 else 0
- */
+* _isdigit - Returns 1 if c is a digit
+* @c: takes in a character
+* Return: 0 for uppercase, 1 for lowercase
+**/
 int _isdigit(int c)
 {
-
-if (isdigit(c))
+if (c >= '0' && c <= '9')
 return (1);
 else
 return (0);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
