4e9715ad92600dbb630d95e4aae55ad9eb05fc87
Yael-Millan <elvinyael16@gmail.com>
2024-02-25 19:19:27 -0400

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - print if the number is positive, zero, or negative
  *
@@ -11,11 +12,11 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-        putchar(ch);
+putchar(ch);
 }
 for (ch = 'A' ; ch <= 'Z' ; ch++)
 {
-        putchar(ch);
+putchar(ch);
 }
 putchar('\n');
 return (0);
