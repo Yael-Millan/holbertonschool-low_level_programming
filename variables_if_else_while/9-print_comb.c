c25fa9ed46c1eee93bbf44e2c9a36f1a2ea08777
Yael-Millan <elvinyael16@gmail.com>
2024-02-25 19:36:50 -0400

    9

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -1,32 +1,70 @@
 #include <stdio.h>
+#include <ctype.h>
+
 /**
- * main - print if the number is positive, zero, or negative
- *
- * Description: using the main function
- * this program prints "Programming is positive, zero, or negative
- * Return: 0
- */
-cat > 9-print_comb.c
-#include <stdio.h>
-/**
- * main - printing numbers from 0-9 with comas and space between them
- * Description: using the main function
- * this program prints "0, 1, 2, 3, 4, 5, 6, 7, 8, 9,"
- * Return: 0
- */
+*main - Prints all single digits with , and space followed by new line
+*
+*Return: returns 0
+*/
 int main(void)
 {
-        int c;
+int digit;
 
-        for (c = 48; c <= 57; c++)
-        {
-                putchar(c);
-                if (c != 57)
-                {
-                       putchar(',');
-                       putchar(' ');
-                }
-        }
-        putchar('\n');
-        return (0);
+for (digit = '0'; digit <= '9'; digit++)
+{
+putchar(digit);
+if (digit == '9')
+continue;
+
+putchar(',');
+putchar(' ');
 }
+putchar('\n');
+return (0);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
