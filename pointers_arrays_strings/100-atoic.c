ad9ed6cf4110f073ccf5fc8a832deb567949a751
Yael-Millan <elvinyael16@gmail.com>
2024-02-26 14:01:29 -0400

    100

--- pointers_arrays_strings/100-atoic.c
+++ pointers_arrays_strings/100-atoic.c
@@ -1,25 +1,44 @@
-#include "main.h"
-
 /**
- * _atoi - Converts a string to an integer.
- * @s: The string to be converted.
- *
- * Return: The integer value of the converted string.
- */
-
+* _atoi - changes a string to an int
+* @s: the string to be changed
+*
+* Return: the converted int
+*/
 int _atoi(char *s)
 {
-int sign = 1;
+int i = 1;
 unsigned int num = 0;
-
 do {
 if (*s == '-')
-sign *= -1;
+i *= -1;
 else if (*s >= '0' && *s <= '9')
-num = (num * 10) + (*s - '0');
+num = num * 10 + (*s - '0');
 else if (num > 0)
 break;
 } while (*s++);
-
-return (num * sign);
+return (num *i);
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
